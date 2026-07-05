f6c16b33fb66b156e00d4040f48cca06dc2a7693
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-07-05 11:40:56 +0530

    fix: improve  error handling and fix syntax error

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,25 +1,27 @@
 import mongoose from "mongoose"
 
-type connectionObject ={
-    isConnected ? : number
+type connectionObject = {
+    isConnected?: number
 }
 
-const connection:connectionObject= {}
+const connection: connectionObject = {}
 
-async function dbConnect():Promise<void>{
+async function dbConnect(): Promise<void> {
     if (connection.isConnected) {
         console.log("connection is already established")
         return
     }
+    if (!process.env.MONGODB_URI) {
+        throw new Error("Please define the MONGODB_URI environment variable");
+    }
 
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI || " ", {})
+        const db = await mongoose.connect(process.env.MONGODB_URI , {})
         connection.isConnected = db.connections[0].readyState;
-         console.log("DB connected successfully");
+        console.log("DB connected successfully");
     } catch (error) {
-        console.log("failed to connect ")
-        process.exit(1)
-        
+       console.error("Database connection failed:", error);
+       throw new Error("Failed to connect to database");
     }
 
 }
